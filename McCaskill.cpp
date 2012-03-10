70aadd8b9091871a356dd3c835ec839aa23e0e16
Evan Senter <evansenter@gmail.com>
2012-03-10 17:51:59 -0500

    Starting to track x

--- McCaskill.cpp
+++ McCaskill.cpp
@@ -54,19 +54,6 @@ dcomplex** runMcCaskill(char sequence[MAXSIZE]) {
   basePairs      = getBasePairList(structure);
   basePairCounts = fillBasePairCounts(basePairs, seqlen);
   
-  // std::cout << "Base pairs." << std::endl;
-  // std::cout << structure << std::endl;
-  // for (i = 0; i <= seqlen; ++i) {
-  //   std::cout << i << ", " << basePairs[i] << std::endl;
-  // }
-  // 
-  // std::cout << "Base pair matrix." << std::endl;
-  // for (d = MIN_PAIR_DIST + 1; d < seqlen; ++d) {
-  //   for (i = 1; i <= seqlen - d; ++i) {
-  //     std::cout << i << ", " << i + d << ": " << basePairCounts[i][i + d] << std::endl;
-  //   }
-  // }
-  
   // Start main recursions.
   for (root = 0; root <= seqlen; ++root) {
     // Flush the matrices.
@@ -90,22 +77,16 @@ dcomplex** runMcCaskill(char sequence[MAXSIZE]) {
         j = i + d;
       
         if (BP(i, j, sequence)) {
-          solveZB(i, j, rootsOfUnity[i][0], sequence, basePairCounts, ZB, ZM);
+          solveZB(i, j, rootsOfUnity[root][0], sequence, basePairs, basePairCounts, ZB, ZM);
         }
         
-        solveZM(i, j, rootsOfUnity[i][0], sequence, basePairCounts, ZB, ZM);
+        solveZM(i, j, rootsOfUnity[root][0], sequence, basePairs, basePairCounts, ZB, ZM);
       
-        solveZ(i, j, rootsOfUnity[i][0], sequence, basePairCounts, Z, ZB);
+        solveZ(i, j, rootsOfUnity[root][0], sequence, basePairs, basePairCounts, Z, ZB);
       }
     }
     
     rootsOfUnity[root][1] = Z[1][seqlen];
-    
-    std::cout << Z[1][seqlen] << std::endl;
-    std::cout << Z[seqlen][1] << std::endl;
-    std::cout << rootsOfUnity[root][0] << std::endl;
-    std::cout << rootsOfUnity[root][1] << std::endl;
-    std::cout << std::endl;
   }
   
   // LaGenMatComplex A(row, col);
@@ -114,8 +95,6 @@ dcomplex** runMcCaskill(char sequence[MAXSIZE]) {
   // 
   // for (i = 0; i < row; i++) {
   //   for (j = 0; j < col; j++) {
-  //     squareMatrix[i][j] = dcomplex(i, j);
-  //     
   //     A(i, j).r = i + j + 0.5;
   //     A(i, j).i = -(i + j + 0.5);
   //   }
@@ -124,37 +103,28 @@ dcomplex** runMcCaskill(char sequence[MAXSIZE]) {
   //   B(i).i = -(i + 0.5);
   // }
   // 
-  // std::cout << A << std::endl;
-  // 
-  // std::complex<double> *x = new std::complex<double>(3, 2);
-  // std::complex<double> *y = new std::complex<double>(4, 5);
-  // 
-  // x + y;
-  // 
-  // std::cout << x -> real() << std::endl;
-  // 
   // LaLinearSolveIP(A, X, B);
   
   return Z;
 }
 
-void solveZ(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **Z, std::complex<double> **ZB) { 
+void solveZ(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **Z, std::complex<double> **ZB) { 
   int k;
   
   if(j - i < MIN_PAIR_DIST + 1) {
     SET_Z(i, j, 1)
   } else {
-    Z[i][j] += Z[i][j - 1];
+    Z[i][j] += Z[i][j - 1] * pow(x, basePairs[j] < j && basePairs[j] >= i ? 1 : 0);
     Z[j][i] += Z[j - 1][i];
     
     for (k = i; k <= j - MIN_PAIR_DIST - 1; ++k) { 
       // (k, j) is the rightmost base pair in (i, j).
       if (BP(k, j, sequence)) {
         if (k == i) {
-          Z[i][j] += ZB[k][j] * exp(-AU_Penalty(i, j, S0) / kT);
-        Z[j][i] += ZB[j][k];
+          Z[i][j] += ZB[k][j] * exp(-AU_Penalty(i, j, S0) / kT) * x;
+          Z[j][i] += ZB[j][k];
         } else {
-          Z[i][j] += Z[i][k - 1] * ZB[k][j] * exp(-AU_Penalty(k, j, S0) / kT);
+          Z[i][j] += Z[i][k - 1] * ZB[k][j] * exp(-AU_Penalty(k, j, S0) / kT) * pow(x, basePairCounts[i][j] - basePairCounts[i][k - 1] - basePairCounts[k][j]);
           Z[j][i] += Z[k - 1][i] * ZB[j][k];
         }
       }
@@ -162,12 +132,12 @@ void solveZ(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCoun
   }
 }
 
-void solveZB(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM) { 
+void solveZB(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM) { 
   // (i, j) assumed to b.p. in here.
   int k, l;
   
   // In a hairpin, (i + 1, j - 1) all unpaired.
-  ZB[i][j] += exp(-HP_Energy(i, j, S0, sequence + 1) / kT);
+  ZB[i][j] += exp(-HP_Energy(i, j, S0, sequence + 1) / kT) * x;
   ZB[j][i] += 1;
   
   // Interior loop / bulge / stack / multiloop.
@@ -176,33 +146,33 @@ void solveZB(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCou
       if (BP(k, l, sequence)) {
         // In interior loop / bulge / stack with (i, j) and (k, l), (i + 1, k - 1) and (l + 1, j - 1)
         // are all unpaired.
-        ZB[i][j] += ZB[k][l] * exp(-IL_Energy(i, j, k, l, S0) / kT);
+        ZB[i][j] += ZB[k][l] * exp(-IL_Energy(i, j, k, l, S0) / kT) * x;
         ZB[j][i] += ZB[l][k];
           
         // If (i, j) is the closing b.p. of a multiloop, and (k, l) is the rightmost base pair, 
         // there is at least one hairpin between (i + 1, k - 1).
-        ZB[i][j] += exp(-(ML_close + MLbasepairAndAUpenalty(j, i, S0)) / kT) * ZB[k][l] * ZM[i + 1][k - 1];
+        ZB[i][j] += exp(-(ML_close + MLbasepairAndAUpenalty(j, i, S0)) / kT) * ZB[k][l] * ZM[i + 1][k - 1] * x;
         ZB[j][i] += ZB[l][k] * ZM[k - 1][i + 1];
       }
     }
   }
 }
 
-void solveZM(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM) { 
+void solveZM(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM) { 
   int k;
   
-  ZM[i][j] += ZM[i][j - 1] * exp(-1 / kT);
+  ZM[i][j] += ZM[i][j - 1] * exp(-1 / kT) * x;
   ZM[j][i] += ZM[j - 1][i];
   
   for (k = i; k <= j - MIN_PAIR_DIST - 1; ++k) {
     if (BP(k, j, sequence)) {
       // Only one stem.
-      ZM[i][j] += ZB[k][j] * exp(-ML_base * (k - i) / kT);
+      ZM[i][j] += ZB[k][j] * exp(-ML_base * (k - i) / kT) * x;
       ZM[j][i] += ZB[j][k];
       
       // k needs to be greater than MIN_PAIR_DIST + 2 from i to fit more than one stem.
       if (k > i) {
-        ZM[i][j] += ZB[k][j] * ZM[i][k - 1] * exp(-ML_base / kT);
+        ZM[i][j] += ZB[k][j] * ZM[i][k - 1] * exp(-ML_base / kT) * x;
         ZM[j][i] += ZB[j][k] * ZM[k - 1][i];
       }
     }
--- McCaskill.h
+++ McCaskill.h
@@ -4,9 +4,9 @@
 
 typedef std::complex<double> dcomplex;
 
-void solveZB(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM);
-void solveZM(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM);
-void solveZ(int i, int j, dcomplex x, char sequence[MAXSIZE], int **basePairCounts, std::complex<double> **Z, std::complex<double> **ZB);
+void solveZB(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM);
+void solveZM(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **ZB, std::complex<double> **ZM);
+void solveZ(int i, int j, dcomplex x, char sequence[MAXSIZE], int *basePairs, int **basePairCounts, std::complex<double> **Z, std::complex<double> **ZB);
 std::complex<double>** runMcCaskill(char sequence[MAXSIZE]);
 int* getBasePairList(char *structure);
 int numberOfBasePairs(int i, int j, int *basePairs);
